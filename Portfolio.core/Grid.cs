441da3e29ee44859dfaa46451efafdfda627b05a
nadavzohar7@gmail.com <nadavzohar7@gmail.com>
2026-01-23 23:59:11 +0900

    Implement core Grid logic and refactor Position to record

--- Portfolio.core/Grid.cs
+++ Portfolio.core/Grid.cs
@@ -8,5 +8,41 @@ namespace Portfolio.core
 {
     internal class Grid
     {
+        private readonly int width=0, height;
+        private Dictionary<Position, Unit> field;
+
+        public Grid(int widthSize, int heightSize)
+        {
+            width = widthSize;
+            height = heightSize;
+            field = new Dictionary<Position, Unit>();
+        }
+
+        public bool IsInsideGrid(Position position)
+        {
+            if (0<=position.X && position.X<width && 0<=position.Y && position.Y<height)
+                return true;
+            return false;
+        }
+
+        public bool IsOccupied(Position position)
+        {
+            if (!IsInsideGrid(position))
+                return true; //outside == ocupied == can't place
+            return field.ContainsKey(position);
+        }
+
+        public bool Place(Position position, Unit unit)
+        {
+            if (!IsOccupied(position))
+            {
+                field[position] = unit;
+                return true;
+            }
+            else
+                return false;
+        }
+
+
     }
 }
