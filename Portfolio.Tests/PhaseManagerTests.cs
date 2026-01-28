6ae8e88b69e127dc1056f804a45ce6075b5cf4c7
nadavzohar7@gmail.com <nadavzohar7@gmail.com>
2026-01-28 19:23:03 +0900

    Add PhaseManager with full unit test coverage

---
+++ Portfolio.Tests/PhaseManagerTests.cs
@@ -0,0 +1,114 @@
+﻿namespace Portfolio.Tests;
+
+using NUnit.Framework.Internal;
+using Portfolio.core;
+
+public class PhaseManagerTests
+{
+    private PhaseManager phaseManager;
+    private Unit testUnit;
+    private Unit testUnit2;
+
+    [SetUp]
+    public void Setup()
+    {
+        phaseManager = new PhaseManager();
+        testUnit = new Unit(20, 5, 2, 1, 3);
+        testUnit2 = new Unit(18, 7, 2, 1, 3);
+    }
+
+    [Test]
+    public void PhaseManager_CorrectSetUp()
+    {
+        Assert.AreEqual(TurnPhase.PlayerTurn, phaseManager.CurrentPhase);
+        Assert.AreEqual(1, phaseManager.TurnCounter);
+    }
+
+    [Test]
+    public void CanMove_NotYetMovedUnit_ReturnsTrue()
+    {
+        Assert.IsTrue(phaseManager.CanMove(testUnit));
+    }
+
+    [Test]
+    public void CanMove_AlreadyMovedUnit_ReturnsFalse()
+    {
+        //Act
+        phaseManager.RegisterMove(testUnit);
+
+        //Assert
+        Assert.IsFalse(phaseManager.CanMove(testUnit));
+    }
+
+    [Test]
+    public void CanMove_NotYetMovedSecondUnit_ReturnsTrue()
+    {
+        //Act
+        phaseManager.RegisterMove(testUnit2);
+
+        //Assert
+        Assert.IsTrue(phaseManager.CanMove(testUnit));
+    }
+
+    [Test]
+    public void CanAttack_NotYetAttackedUnit_ReturnsTrue()
+    {
+        Assert.IsTrue(phaseManager.CanAttack(testUnit));
+    }
+
+    [Test]
+    public void CanAttack_AlreadyAttackedUnit_ReturnsFalse()
+    {
+        //Act
+        phaseManager.RegisterAttack(testUnit);
+
+        //Assert
+        Assert.IsFalse(phaseManager.CanAttack(testUnit));
+    }
+
+    [Test]
+    public void CanAttack_NotYetAttackedSecondUnit_ReturnsTrue()
+    {
+        //Act
+        phaseManager.RegisterAttack(testUnit2);
+
+        //Assert
+        Assert.IsTrue(phaseManager.CanAttack(testUnit));
+    }
+
+    [Test]
+    public void CanAttack_MovedWithoutAttacking_ReturnsTrue()
+    {
+        //Act
+        phaseManager.RegisterMove(testUnit);
+
+        //Assert
+        Assert.IsTrue(phaseManager.CanAttack(testUnit));
+    }
+
+    [Test]
+    public void EndTurn_ResetsActions_IncrementsTurn_AdvancesPhase()
+    {
+        //Act
+        phaseManager.RegisterMove(testUnit);
+        phaseManager.RegisterAttack(testUnit);
+        phaseManager.EndTurn();
+
+        //Assert
+        Assert.IsTrue(phaseManager.CanMove(testUnit));
+        Assert.IsTrue(phaseManager.CanAttack(testUnit));
+        Assert.AreEqual(2, phaseManager.TurnCounter);
+        Assert.AreEqual(TurnPhase.EnemyTurn, phaseManager.CurrentPhase);
+    }
+
+    [Test]
+    public void EndTurn_EnemyTurn_ReturnsToPlayerTurn()
+    {
+        //Act
+        phaseManager.EndTurn();
+        phaseManager.EndTurn();
+
+        //Assert
+        Assert.AreEqual(TurnPhase.PlayerTurn, phaseManager.CurrentPhase);
+    }
+}
---
+++ Portfolio.core/PhaseManager.cs
@@ -0,0 +1,60 @@
+﻿namespace Portfolio.core
+{
+    public enum TurnPhase
+    {
+        PlayerTurn, 
+        EnemyTurn
+    }
+    public class PhaseManager
+    {
+        private int turnCounter = 1;
+        private TurnPhase turnPhase = TurnPhase.PlayerTurn;
+        private HashSet<Unit> moved = new HashSet<Unit>();
+        private HashSet<Unit> attacked = new HashSet<Unit>();
+        //no constructor because the game must start in turn 1, player phase. later consider constructor for turnPhase in cases of enemy start levels
+
+        public int TurnCounter => turnCounter;
+        public TurnPhase CurrentPhase => turnPhase;
+
+        public bool CanMove(Unit unit)
+        {
+            return !moved.Contains(unit);
+        } 
+
+        public bool CanAttack(Unit unit)
+        {
+            return !attacked.Contains(unit);
+        }
+
+        public void RegisterMove(Unit unit)
+        {
+            moved.Add(unit);
+        }
+
+        public void RegisterAttack(Unit unit)
+        {
+            attacked.Add(unit);
+        }
+
+        private void AdvancePhase()
+        {
+            switch (turnPhase)
+            {
+                case TurnPhase.PlayerTurn:
+                    turnPhase = TurnPhase.EnemyTurn;
+                    break;
+                case TurnPhase.EnemyTurn:
+                    turnPhase = TurnPhase.PlayerTurn;
+                    break;
+            }
+        }
+
+        public void EndTurn()
+        {
+            attacked.Clear();
+            moved.Clear();
+            turnCounter++;
+            AdvancePhase();
+        }
+    }
+}
